e914fded72d152ef7cbc3ac2df4956bf2b93591b
Imran Razak <77019152+ImranRazak1@users.noreply.github.com>
2024-02-07 16:40:50 +0000

    Initial Commit

---
+++ ProjectMouse/ProjectMouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectMouseApp.swift
+//  ProjectMouse
+//
+//  Created by Imran razak on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjectMouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
